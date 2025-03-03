d3fd97c50a56282d564b175215d60e0656833950
tinaylt <144871562+tinaylt@users.noreply.github.com>
2025-03-03 01:42:53 -0500

    sendform.php adjustment

--- sendfrom.php
+++ sendfrom.php
@@ -13,7 +13,7 @@
     $fname = trim($fname);
     $lname = trim($lname);
     $email = trim($email);
-    $msg = trim($message); // Ensure $msg is used consistently for the message field
+    $msg = trim($message);
     
     if(empty($fname)) {
         $errors[] = 'Please fill in your First Name';
@@ -29,36 +29,32 @@
         $errors[] = 'I know this is not your REAL email.';
     }
 
-    if(empty($msg)) { // Check $msg (which is trimmed $message)
+    if(empty($msg)) {
         $errors[] = 'Please leave a message for me';
     }
-
-    // Check for errors
+    
     $errorcount = count($errors);
     
     if($errorcount > 0) {
         echo json_encode(array("errors" => $errors));
     } else {
-        // Prepare the SQL query
         $query = "INSERT INTO contacts (fname, lname, email, message) VALUES (:fname, :lname, :email, :msg)";
         $stmt = $connection->prepare($query);
         $stmt->bindParam(':fname', $fname);
         $stmt->bindParam(':lname', $lname);
         $stmt->bindParam(':email', $email);
-        $stmt->bindParam(':msg', $msg); // Use $msg here
+        $stmt->bindParam(':msg', $msg); 
         
         $stmt->execute();
 
-        // Email setup
         $to = 'l_yam203842@fanshaweonline.ca';
         $subject = 'You got a message from your portfolio site!';
         
         $email_message = "You have received a new contact form submission:\n\n";
         $email_message .= "Name: ".$fname." ".$lname."\n";
         $email_message .= "Email: ".$email."\n\n";
-        $email_message .= "Message:\n" . $msg; // Use $msg for the email message content
+        $email_message .= "Message:\n" . $msg;
 
-        // Send email and check result
         if(mail($to, $subject, $email_message)) {
             echo json_encode(array("message" => "Form submitted. Thank you for your interest!"));
         };
